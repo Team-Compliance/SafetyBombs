293e4ff0de1c70202057e3b8eb132f35ef63b856
kittenchilly <mario.pikmin.zelda@gmail.com>
2022-04-22 10:32:17 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,79 +7,6 @@ if EID then
     EID:addCollectible(CollectibleType.COLLECTIBLE_SAFETY_BOMBS, "{{Bomb}} +5 Bombs#Placed bombs will not explode until the player leaves its explosion radius", "Safety Bombs")
 end
 
-local function getBombRadiusFromDamage(damage)
-	if 175.0 <= damage then
-		return 105.0
-	else
-		if damage <= 140.0 then
-			return 75.0
-		else
-			return 90.0
-		end
-	end
-end
-
-function mod:BombUpdate(bomb)
-	local player = mod:GetPlayerFromTear(bomb)
-	if player then
-		if bomb.Type == EntityType.ENTITY_BOMB then
-			if bomb.Variant ~= BombVariant.BOMB_THROWABLE then
-				if player:HasCollectible(CollectibleType.COLLECTIBLE_SAFETY_BOMBS) then
-					local sprite = bomb:GetSprite()
-					
-					if bomb.FrameCount == 1 then
-						if bomb.Variant == BombVariant.BOMB_NORMAL then
-							if not bomb:HasTearFlags(TearFlags.TEAR_BRIMSTONE_BOMB) then
-								if bomb:HasTearFlags(TearFlags.TEAR_GOLDEN_BOMB) then
-									sprite:ReplaceSpritesheet(0, "gfx/items/pick ups/bombs/costumes/safety_bombs_gold.png")
-								else
-									sprite:ReplaceSpritesheet(0, "gfx/items/pick ups/bombs/costumes/safety_bombs.png")
-								end
-								sprite:LoadGraphics()
-							end
-						end
-					end
-					for i, player in ipairs(Isaac.FindInRadius(bomb.Position, getBombRadiusFromDamage(bomb.ExplosionDamage) * bomb.RadiusMultiplier, EntityPartition.PLAYER)) do
-						bomb:SetExplosionCountdown(45)
-						break
-					end
-				end
-			end
-		end
-	end
-end
-mod:AddCallback(ModCallbacks.MC_POST_BOMB_UPDATE, mod.BombUpdate)
-
--------
-
-function mod:GetPlayerFromTear(tear)
-	local check = tear.Parent or tear.SpawnerEntity
-	if check then
-		if check.Type == EntityType.ENTITY_PLAYER then
-			return mod:GetPtrHashEntity(check):ToPlayer()
-		elseif check.Type == EntityType.ENTITY_FAMILIAR and check.Variant == FamiliarVariant.INCUBUS then
-			local data = tear:GetData()
-			data.IsIncubusTear = true
-			return mod:GetPtrHashEntity(check:ToFamiliar().Player):ToPlayer()
-		end
-	end
-	return nil
-end
-
-function mod:GetPtrHashEntity(entity)
-	if entity then
-		if entity.Entity then
-			entity = entity.Entity
-		end
-		for _, matchEntity in pairs(Isaac.FindByType(entity.Type, entity.Variant, entity.SubType, false, false)) do
-			if GetPtrHash(entity) == GetPtrHash(matchEntity) then
-				return matchEntity
-			end
-		end
-	end
-	return nil
-end
-
 local Wiki = {
   SafetyBombs = {
     { -- Effect
@@ -106,3 +33,75 @@ if Encyclopedia then
 	  },
 	})
 end
+
+local function GetPtrHashEntity(entity)
+	if entity then
+		if entity.Entity then
+			entity = entity.Entity
+		end
+		for _, matchEntity in pairs(Isaac.FindByType(entity.Type, entity.Variant, entity.SubType, false, false)) do
+			if GetPtrHash(entity) == GetPtrHash(matchEntity) then
+				return matchEntity
+			end
+		end
+	end
+	return nil
+end
+
+local function GetPlayerFromTear(tear)
+	local check = tear.Parent or tear.SpawnerEntity
+	if check then
+		if check.Type == EntityType.ENTITY_PLAYER then
+			return GetPtrHashEntity(check):ToPlayer()
+		elseif check.Type == EntityType.ENTITY_FAMILIAR and check.Variant == FamiliarVariant.INCUBUS then
+			local data = tear:GetData()
+			data.IsIncubusTear = true
+			return GetPtrHashEntity(check:ToFamiliar().Player):ToPlayer()
+		end
+	end
+	return nil
+end
+
+local function getBombRadiusFromDamage(damage)
+	if 175.0 <= damage then
+		return 105.0
+	else
+		if damage <= 140.0 then
+			return 75.0
+		else
+			return 90.0
+		end
+	end
+end
+
+function mod:BombUpdate(bomb)
+	local player = GetPlayerFromTear(bomb)
+	if player then
+		if bomb.Type == EntityType.ENTITY_BOMB then
+			if bomb.Variant ~= BombVariant.BOMB_THROWABLE then
+				if player:HasCollectible(CollectibleType.COLLECTIBLE_SAFETY_BOMBS) then
+					local sprite = bomb:GetSprite()
+					
+					if bomb.FrameCount == 1 then
+						if bomb.Variant == BombVariant.BOMB_NORMAL then
+							if not bomb:HasTearFlags(TearFlags.TEAR_BRIMSTONE_BOMB) then
+								if bomb:HasTearFlags(TearFlags.TEAR_GOLDEN_BOMB) then
+									sprite:ReplaceSpritesheet(0, "gfx/items/pick ups/bombs/costumes/safety_bombs_gold.png")
+								else
+									sprite:ReplaceSpritesheet(0, "gfx/items/pick ups/bombs/costumes/safety_bombs.png")
+								end
+								sprite:LoadGraphics()
+							end
+						end
+					end
+
+					for i, player in ipairs(Isaac.FindInRadius(bomb.Position, getBombRadiusFromDamage(bomb.ExplosionDamage) * bomb.RadiusMultiplier, EntityPartition.PLAYER)) do
+						bomb:SetExplosionCountdown(45)
+						break
+					end
+				end
+			end
+		end
+	end
+end
+mod:AddCallback(ModCallbacks.MC_POST_BOMB_UPDATE, mod.BombUpdate)
